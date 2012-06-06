de06807ad95232cd874ce60f8ee692f2e7c6fe74
lbertelo <berteloot.luc@free.fr>
2012-06-06 11:27:08 +0200

    AnimalFactory : Paramètre du poids aléatoire Animal : vérification et maj des algos avec toCase et toStep Animal : déplacement de la méthode moveTo Herbivorious : changement de la détection de nourriture considérée comme mangeable

--- Implementation/Workspace/Simulation/src/preSimulationWindow/View.java
+++ Implementation/Workspace/Simulation/src/preSimulationWindow/View.java
@@ -1,30 +1,9 @@
 package preSimulationWindow;
 
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-
-import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
-import javax.swing.DefaultCellEditor;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JComboBox;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
-import javax.swing.JTable;
-import javax.swing.JTextField;
-import javax.swing.border.Border;
 
-import simulation.SimProperties;
 import utils.Constants;
 
 @SuppressWarnings("serial")
--- Implementation/Workspace/Simulation/src/simulation/entity/Herbivorious.java
+++ Implementation/Workspace/Simulation/src/simulation/entity/Herbivorious.java
@@ -20,8 +20,7 @@ public class Herbivorious extends Animal {
 	public void  initializePerception(){
 		//System.out.println(this.getType()+" #"+this.hashCode()+" perception initialized !");
 
-		int perceptionPoint = (int) Math.max(smellPoint, visionPoint);
-		
+		int perceptionPoint = (int) Math.round(toCase(Math.max(smellPoint, visionPoint)));
 		
 		//Initialize perception table
 		perception = new PerceptionTableCell[2*perceptionPoint][2*perceptionPoint];
@@ -40,29 +39,29 @@ public class Herbivorious extends Animal {
 		
 		Integer[] coor = getNearbyFood();
 		if (weight < weightConsumeByDay * maxNbDaySafe) {
-			//System.out.println(this.getType()+" #"+this.hashCode()+" is on ("+getX()+","+getY()+") and is hungry !");
+			System.out.println(this.getType()+" #"+this.hashCode()+" is on ("+getX()+","+getY()+") and is hungry !");
 
 			if((coor[0]!=-1)&&(coor[1]!=-1)){
 				if(isOnFood()){
-					//System.out.println(this.getType()+" #"+this.hashCode()+" and is on food !");
+					System.out.println(this.getType()+" #"+this.hashCode()+" and is on food !");
 					eat((int)getX(),(int)getY(),simModel.getVegetation(),simModel);
 				}else{
-					//System.out.println(this.getType()+" #"+this.hashCode()+" and sees food on ("+coor[0]+","+coor[1]+")/("+simModel.getVegetationAt(coor[0], coor[1])+") !");
+					System.out.println(this.getType()+" #"+this.hashCode()+" and sees food on ("+coor[0]+","+coor[1]+")/("+simModel.getVegetationAt(coor[0], coor[1])+") !");
 					moveTo(yard,coor);
 				}
 			}else{
 				if(!isOnFood()){
-					//System.out.println(this.getType()+" #"+this.hashCode()+" and doesn't see food !");
+					System.out.println(this.getType()+" #"+this.hashCode()+" and doesn't see food !");
 					moveRandom(yard);
 				}else{
-					//System.out.println(this.getType()+" #"+this.hashCode()+" and is on food !");
+					System.out.println(this.getType()+" #"+this.hashCode()+" and is on food !");
 					eat((int)getX(),(int)getY(),simModel.getVegetation(),simModel);
 				}
 				
 
 			}
 			}else{
-				//System.out.println(this.getType()+" #"+this.hashCode()+" is not hungry and is on ("+getX()+","+getY()+") !");
+				System.out.println(this.getType()+" #"+this.hashCode()+" is not hungry and is on ("+getX()+","+getY()+") !");
 				moveRandom(yard);
 			}
 }
@@ -70,7 +69,7 @@ public class Herbivorious extends Animal {
 	private void detectFood(SparseGrid2D yard,Float[][] Vegetation, SimulationModel simModel) {
 		
 		//Parse surrounding to get vegetation
-		int perceptionPoint = (int) Math.max(smellPoint, visionPoint);
+		int perceptionPoint = (int) Math.round(toCase(Math.max(smellPoint, visionPoint)));
 		
 
 		
@@ -107,12 +106,14 @@ public class Herbivorious extends Animal {
 	private Integer[] getNearbyFood() {
 		ArrayList<Int2D> cases = new ArrayList<Int2D>();
 
-		double minDistance=movePoint;
+		double minDistance=toCase(movePoint);
 		
 		double currentDistance;
 		for(PerceptionTableCell[] i:perception){
 			for(PerceptionTableCell j:i){
-				if(j.getAmount()>0){
+				// un herbivore considère l'herbe mangeable quand il peut manger au moins
+				// la moitié d'une portion normale (portion normale = toStep(weightConsomeByDay)*2)
+				if(j.getAmount() >= toStep(weightConsumeByDay)){
 					currentDistance = getShortestDistanceToPoint(j.getX(), j.getY());
 					if(currentDistance<minDistance){ //if nearer food found
 						cases.clear();
@@ -139,50 +140,7 @@ public class Herbivorious extends Animal {
 			i[1]=d.y;
 			return i;
 		}
-	}
-	
-	private Double moveTo(SparseGrid2D yard, Integer[] coordinates) {
-		Double move = movePoint;
-		
-		//System.out.println(this.getType()+" #"+this.hashCode()+" trying to move to ("+coordinates[0]+","+coordinates[1]+") and movePoint ("+movePoint+")");
-
-		
-		
-		
-		
-		if((coordinates[0]!=-1)&&(coordinates[1]!=-1)){//if valid coordinates
-		
-			while (move > 0 && !isSameLocation(coordinates[0],coordinates[1])) {
-				int dX = getXShortestDirection(coordinates[0]);
-				if (dX > 0){
-					setX(simModel.getYard().stx((int) (this.getX() + 1)));
-					//System.out.println("x+1 -> "+this.getX());
-					
-				}else if (dX < 0){
-					setX(simModel.getYard().stx((int) (this.getX() - 1)));
-					//System.out.println("x-1 -> "+this.getX());
-				}
-
-				int dY = getYShortestDirection(coordinates[1]);
-				if (dY > 0){
-					setY(simModel.getYard().sty((int)getY() + 1));
-					//System.out.println("y+1 -> "+this.getY());
-					
-				}else if (dY < 0){
-					setY(simModel.getYard().sty((int)getY() - 1));
-					//System.out.println("y-1 -> "+this.getY());
-				}
-
-				move--;
-			}
-			return (movePoint - move);
-		}else{
-			return movePoint;
-		}
-		
-
-	}
-	
+	}	
 	
 	private Double moveRandom(SparseGrid2D yard) {
 		float randomSign = (float) (Math.random()*2);
@@ -192,8 +150,8 @@ public class Herbivorious extends Animal {
 		}
 		
 		//maximum distance toward random direction 
-		float dX = (float) (Math.cos(randomAngle)*movePoint);
-		float dY = (float) (Math.sin(randomAngle)*movePoint);
+		float dX = (float) (Math.cos(randomAngle)*toCase(movePoint));
+		float dY = (float) (Math.sin(randomAngle)*toCase(movePoint));
 		
 		
 		//System.out.println(this.getType()+" #"+this.hashCode()+"  ("+movePoint+") move randomly toward "+Math.toDegrees(randomAngle)+"+ degrees ("+Math.cos(randomAngle)+","+Math.sin(randomAngle)+") to ("+dX+","+dY+") from itself!");
@@ -235,7 +193,8 @@ public class Herbivorious extends Animal {
 	private void eat(int VegetationX, int VegetationY, Float[][] floats, SimulationModel SimModel) {
 		//consume vegetationand update weight
 
-		float consumes=SimModel.consumeVegetationAt(VegetationX, VegetationY, toStep(weightConsumeByDay));
+		// * 2 en supposant qu'un herbivore mange la moitié de son temps
+		float consumes=SimModel.consumeVegetationAt(VegetationX, VegetationY, toStep(weightConsumeByDay)*2);
 		
 		//System.out.println(this.getType()+" #"+this.hashCode()+" tries to eat "+(toStep(weightConsumeByDay)*1000)+"g of  grass !");
 
@@ -245,7 +204,9 @@ public class Herbivorious extends Animal {
 	}
 	
 	public boolean isOnFood(){
-		if(simModel.getVegetationAt((int)getX(), (int)getY())>0){
+		// un herbivore considère l'herbe mangeable quand il peut manger au moins
+		// la moitié d'une portion normale (portion normale = toStep(weightConsomeByDay)*2)
+		if(simModel.getVegetationAt((int)getX(), (int)getY())> toStep(weightConsumeByDay)){
 			return true;
 		}else{
 			return false;
@@ -253,11 +214,11 @@ public class Herbivorious extends Animal {
 	}
 	
 	public void printPerception(){
-		int perceptionPoint = (int) Math.max(smellPoint, visionPoint);
+		int perceptionPointCase = (int)Math.round(toCase(Math.max(smellPoint, visionPoint)));
 
-		for(int i=0;i<perceptionPoint*2;i++){
-			for(int j=0;j<perceptionPoint*2;j++){
-				if((i==perceptionPoint)&&(j==perceptionPoint)){
+		for(int i=0;i<perceptionPointCase*2;i++){
+			for(int j=0;j<perceptionPointCase*2;j++){
+				if((i==perceptionPointCase)&&(j==perceptionPointCase)){
 					System.out.print("X ");
 				}else{
 					System.out.print(perception[i][j].getAmount()+" ");	
